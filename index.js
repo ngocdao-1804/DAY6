907480d326fbe26a47af40b2dc27926bbabfd3ba
ngocdao-1804 <accloneofdao@gmail.com>
2026-02-26 21:21:00 +0700

    bai3

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 
+//return
+// hàm calculatePytago
+// 2 tham số: canhgocvuong1, canhGocvuong2
+// tính cạnh huyền
+// mũ: Math.pow(digit, digit)
+// căn: Math.squrt()/
+function calculatePytago(canhGocvuong1,canhGocvuong2){
+    let canhHuyen = Math.sqrt(Math.pow(canhGocvuong1,canhGocvuong2));
+    return canhHuyen;
+}
+console.log(calculatePytago(3,4));
+
+
+
+// Function calculation (length, width) {
+//     let area = width * length;
+//     return area
+// }
+// console.log(calculationArea(3,5))
+
 //bai 2: 1 hàm introduce
 // Nhận 3 tham số: name, age, address
 // In: Xin chào, tôi tên là ...
 // Năm nay tôi .. tuổi
 // Tôi đang sinh sống ở//
-function introduce(name, age, address){
-    console.log(`Xin chào, tôi tên là ${name}, năm nay tôi ${age} tuổi, tôi đang sinh sống ở ${address} `);
-}
-introduce(`Đào`, 20, `thành phố Hồ Chí Minh`)
+// function introduce(name, age, address){
+//     console.log(`Xin chào, tôi tên là ${name}, năm nay tôi ${age} tuổi, tôi đang sinh sống ở ${address} `);
+// }
+// introduce(`Đào`, 20, `thành phố Hồ Chí Minh`)
 
 // bai 1 hàm printName
 // 1 tham số tên name
