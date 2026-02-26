992ea253e867bf9332e1e54bc3f2c2c0c7217a70
ngocdao-1804 <accloneofdao@gmail.com>
2026-02-26 20:23:26 +0700

    bai2

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+
+//1 hàm introduce
+// Nhận 3 tham số: name, age, address
+// In: Xin chào, tôi tên là ...
+// Năm nay tôi .. tuổi
+// Tôi đang sinh sống ở//
+function introduce(name, age, address){
+    console.log(`Xin chào, tôi tên là ${name}, năm nay tôi ${age} tuổi, tôi đang sinh sống ở ${address} `);
+}
+introduce(`Đào`, 20, `thành phố Hồ Chí Minh`)
+
 // hàm printName
 // 1 tham số tên name
 // trong hàm in ra name đó
-function printName (name){
-    console.log(`Hello ${name}`);
-}
-printName(`Dao92`);
-printName(`Lucy`);
+// function printName(name){
+//     console.log(`Hello ${name}`);
+// }
+// printName(`Dao92`);
+// printName(`Lucy`);
